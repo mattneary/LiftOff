357761d3a671961d91e6f1cca1c313acc456abf5
Matt Neary <neary.matt@gmail.com>
2013-08-14 00:24:59 -0400

    middleware architecure, the utility has yet to be designed

--- server.rb
+++ server.rb
@@ -1,18 +1,22 @@
 require 'rack'
 
 class LiftOff
-  attr_accessor :defm, :builder
+  attr_accessor :builder
   def initialize
     defm = Object.method(:define_method)
     builder = Rack::Builder.new
+    middleware = {}
     %w[get post put delete].map{ |m|
       defm.(m) { |u,&b|
         builder.map(u) {
 	  run ->(e) {
-	    [200, {"Content-Type"=>"text/html"}, [builder.instance_eval(&b)]]
+	    [200, {"Content-Type"=>"text/html"}, [builder.instance_eval(&middleware[u]).instance_eval(&b)]]
 	  }
 	}
       }
+    }
+    defm.("middle") { |p,&b|
+      middleware[p] = b
     }
     @builder = builder
   end
@@ -20,10 +24,16 @@ end
 
 server = LiftOff.new.builder
 
+middle '/' do
+  puts "middle"
+end
 get '/' do
   "Hello"
 end
 
+middle '/bye' do
+  puts "middle"
+end
 post '/bye' do
   "Goodbye"
 end
